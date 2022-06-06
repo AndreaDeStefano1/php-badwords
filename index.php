97bde56d6634e42e5ae543353ebbc6d2f5fd4f0d
Andrea.DeStefano <enricoandrea.destefano@gmail.com>
2022-06-06 15:50:25 +0200

    Fine esercizio

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php 
-$testo = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio atque inventore ipsum expedita iure obcaecati magnam, libero autem quam repudiandae laborum. Expedita soluta repellendus facilis exercitationem! Necessitatibus quasi vitae consectetur iste eos debitis voluptatem eius, labore quod iure maxime voluptate ipsa laborum. Praesentium officiis reiciendis atque expedita, suscipit molestias fugiat. Doloremque distinctio praesentium, deleniti doloribus voluptates sit aspernatur! Magni quidem dolores sed, natus ratione culpa quisquam odio a laboriosam enim, aliquam eum maxime possimus! Aliquam, nobis assumenda? Beatae, necessitatibus? Quam, ex facilis quaerat magnam vel sed neque porro nesciunt a nisi? Quis iure deleniti debitis quasi, impedit molestiae tempore quos.'
+$text = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio atque inventore ipsum expedita iure obcaecati magnam, libero autem quam repudiandae laborum. Expedita soluta repellendus facilis exercitationem! Necessitatibus quasi vitae consectetur iste eos debitis voluptatem eius, labore quod iure maxime voluptate ipsa laborum. Praesentium officiis reiciendis atque expedita, suscipit molestias fugiat. Doloremque distinctio praesentium, deleniti doloribus voluptates sit aspernatur! Magni quidem dolores sed, natus ratione culpa quisquam odio a laboriosam enim, aliquam eum maxime possimus! Aliquam, nobis assumenda? Beatae, necessitatibus? Quam, ex facilis quaerat magnam vel sed neque porro nesciunt a nisi? Quis iure deleniti debitis quasi, impedit molestiae tempore quos.';
+
+$badWord = $_GET["parola"];
+$newText =  str_replace($badWord, '***', $text );
+
 ?>
 
 
@@ -15,9 +19,9 @@ $testo = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio atque i
 
 <h1>Testo:</h1>
 <p>
-  <?php echo $testo ?>  
+  <?php echo $newText ?>  
 </p>
-<h1> La lunghezza del testo è : <span><?php echo strlen($testo)?></span></h1>
+<h1> La lunghezza del testo è : <span><?php echo strlen($newText)?></span></h1>
 
 </body>
 </html>
